0b634439ef2e72448a6ddc82b6ba35adabe89e45
WALLACE DE OLIVEIRA MARTINS <WALLACE.MARTINS@csi.aneas.msft>
2019-03-21 19:38:10 -0300

    Atualização em aula

--- gravar.php
+++ gravar.php
@@ -1,5 +1,16 @@
 <?php 
-$nome=
-$email=
+
+$nome        = $_POST["nome"];
+$email       = $_POST["email"];
+$estadocivil = $_POST["estadocivil"];
+$sexo        = $_POST["sexo"];
+
+echo"Nome: " . $nome;
+echo"<br>Email: " . $email;
+echo"<br>Estado Civil: " . $estadocivil;
+echo"<br>Sexo: " . $sexo;
+
+
+
 
 ?>
