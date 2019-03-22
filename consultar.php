98e4a49af0eb9466214e6caacf9330f08816f698
WALLACE DE OLIVEIRA MARTINS <WALLACE.MARTINS@csi.aneas.msft>
2019-03-21 21:39:18 -0300

    Finalização da aula 21-03-19

---
+++ consultar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Amazon</title>
+    
+</head>
+<body>
+    
+    <div class="container">
+
+        <h3>Consultar Clientes</h3>
+        
+        <form>
+            Nome:
+            <input type="text" name="nome" class="form-control">
+            <input type="submit" value="Buscar" class="btn btn-info">
+        </form>
+        
+    </div>
+</body>
+</html>   
--- gravar.php
+++ gravar.php
@@ -5,8 +5,22 @@ $email       = $_POST["email"];
 $estadocivil = $_POST["estadocivil"];
 $sexo        = $_POST["sexo"];
 
-include_once 'conexão.php';
+include_once 'conexao.php';
     
-"INSERT INTO clientes VALUES(null, '$nome', '$email', '$estadocivil', '$sexo')";    
+$sql = "INSERT INTO clientes VALUES(null, '$nome', '$email', '$estadocivil', '$sexo')";
+
+if(mysqli_query($con, $sql)){
+    echo"Cliente gravado com sucesso!";
+}else{
+    echo"Erro ao gravar cliente!  <br>";
+    echo "Erro: " . mysqli_error($con);
+}
+mysqli_close($con);
 
 ?>
+
+
+<html>
+    <br><br>
+    <a href="index.html">Página inicial</a>
+</html>
